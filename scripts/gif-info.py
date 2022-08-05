bfbcea9a5135cee6698006b5026689fb882ba6f8
SolidHal <hal@halemmerich.com>
2022-08-04 19:12:36 -0500

    clean up imports, TOODs

--- scripts/gif-info.py
+++ scripts/gif-info.py
@@ -4,12 +4,8 @@ import sys
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image, GifImagePlugin, ImageDraw
-from threading import Thread, Event
-from queue import Queue
 import asyncio
 import logging
-import copy
-import concurrent.futures
 
 import config
 import image
@@ -25,13 +21,15 @@ from epoch import Epoch
 # when time moves forward, show the next minute and start computing
 # frames for the next minute
 
-logging.basicConfig(level=logging.INFO, format='(%(threadName)-9s) %(message)s',)
+logging.basicConfig(level=logging.INFO,)
 
 async def gif_info(image_file):
     gif = Image.open(image_file)
     matrix = config.Matrix()
     # pre process our frames so we can dedicate our resources to displaying them later
     fill_matrix = False
+    # get the dominant colors before converting the gif to avoid
+    # the padding from altering which colors are dominant
     dom_colors = color.dominant_colors_gif(gif)
     frames = image.centerfit_gif(gif, matrix, fill_matrix)
     # Close the gif file to save memory now that we have copied out all of the frames
@@ -75,38 +73,30 @@ async def gif_info(image_file):
     async def prepare_canvases(frames, canvases_queue, epoch, ready_event):
         logging.info("Starting prepare_canvases loop")
 
-        #TODO put this in a function
-        canvases = await image.frames_to_canvases(frames, matrix, overlays=[overlay.overlay_clock], overlay_args=(dom_colors, epoch))
-        logging.info(f"Putting canvases on the queue at epoch {epoch}")
-        await canvases_queue.put(canvases)
-        logging.info(f"Put canvases on the queue at epoch {epoch}")
-        epoch.next()
-
-        canvases = await image.frames_to_canvases(frames, matrix, overlays=[overlay.overlay_clock], overlay_args=(dom_colors, epoch))
-        logging.info(f"Putting canvases on the queue at epoch {epoch}")
-        await canvases_queue.put(canvases)
-        logging.info(f"Put canvases on the queue at epoch {epoch}")
-        epoch.next()
-
-        logging.info("Setting ready")
-        ready_event.set()
-
-        while(True):
+        async def enqueue():
             canvases = await image.frames_to_canvases(frames, matrix, overlays=[overlay.overlay_clock], overlay_args=(dom_colors, epoch))
             logging.info(f"Putting canvases on the queue at epoch {epoch}")
             await canvases_queue.put(canvases)
             logging.info(f"Put canvases on the queue at epoch {epoch}")
             epoch.next()
 
+        # prepare 2 epochs before we let the consumer know we are ready
+        # this is to handle the case where we start at 11:40:59
+        # and the consumer immediately needs the next epoch
+        await enqueue()
+        await enqueue()
+
+        logging.info("Setting ready")
+        ready_event.set()
+
+        while(True):
+            await enqueue()
+
     #TODO provide way to pre-process gifs and load/store them on disk
     try:
         print("Press CTRL-C to stop.")
         # need a queue big enough to handle our producer thread not getting scheduled for long
         # periods, but if its too big we waste memory
-
-        #TODO when handling large gifs (like lighthouse)
-        # the producer ends up unscheduled for long periods
-        # which means the consumer eventually drains the queue
         canvases_queue = asyncio.Queue(5)
         ready_event = asyncio.Event()
 
